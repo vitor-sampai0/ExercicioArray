3e7381eb43b7ab6857452ae68ac8396cfc9f3b0c
vitor-sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-03-14 10:56:40 -0300

    método pop

--- exercicioArray.js
+++ exercicioArray.js
@@ -3,7 +3,9 @@ let ListaNumeros = [1, 2, 3]
 //declaro variavel Array
 let Games = new Array ("valorant", "fortnite", "gta")
 
-console.log(Games[1]);
 
-ListaNumeros.push(4,5)
-Games.push("Minecraft", "Fifa")
+
+ListaNumeros.pop()
+Games.pop()
+
+console.log(Games)
