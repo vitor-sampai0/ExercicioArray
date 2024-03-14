8bf055b2d3d821814c7ac5ce977f3ba2c94a5752
vitor-sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-03-14 10:53:46 -0300

    método push

--- exercicioArray.js
+++ exercicioArray.js
@@ -1,1 +1,9 @@
-//oi
+//declaro variavel ListaNumeros
+let ListaNumeros = [1, 2, 3]
+//declaro variavel Array
+let Games = new Array ("valorant", "fortnite", "gta")
+
+console.log(Games[1]);
+
+ListaNumeros.push(4,5)
+Games.push("Minecraft", "Fifa")
